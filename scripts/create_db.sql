49acf58dc0611d4085ae48e9033d6015b65fbff3
Alexandre L'Erario <alerario@gmail.com>
2017-10-30 11:02:19 -0200

    ajuste de arquivos e exclusao de arquivos duplicatos

--- scripts/create_db.sql
+++
@@ -1,8 +0,0 @@
---DROP DATABASE demoiselledb;
-
-CREATE DATABASE demoiselledb
-  WITH ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'C'
-       LC_CTYPE = 'C'
-       CONNECTION LIMIT = -1;
--- scripts/create_tables.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE public.cidade
-(
-  idcidade integer NOT NULL,
-  nome character varying(40),
-  CONSTRAINT pk_cid PRIMARY KEY (idcidade)
-)
