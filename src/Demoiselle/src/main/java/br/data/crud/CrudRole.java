951a339bfc110601c96b25f08e4912352558a05f
Alexandre L'Erario <alerario@gmail.com>
2017-11-15 16:19:22 -0200

    Adicionada funcionalidade crud role

---
+++ src/Demoiselle/src/main/java/br/data/crud/CrudRole.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.data.crud;
+
+
+import br.data.entity.Role;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author alexandrelerario
+ */
+public class CrudRole extends AbstractCrud<br.data.entity.Role> {
+
+    private EntityManager em;
+
+    private static final String PU = EMNames.EMN1;
+
+    public CrudRole() {
+        super(Role.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        if (em == null) {
+            em = Persistence.createEntityManagerFactory(PU).createEntityManager();
+        }
+        return em;
+    }
+
+}
