911eb7efa981c63eba758f4548964eeff5869c3d
Alexandre L'Erario <alerario@gmail.com>
2017-11-15 16:24:49 -0200

    adicionado ; - correcao de erro em create_tables.sql

--- scripts/database/create_tables.sql
+++ scripts/database/create_tables.sql
@@ -3,7 +3,7 @@
   idcidade integer NOT NULL,
   nome character varying(40),
   CONSTRAINT pk_cid PRIMARY KEY (idcidade)
-)
+);
 
 
 CREATE TABLE public.role
@@ -12,4 +12,4 @@ CREATE TABLE public.role
   nome character varying(30),
   descricao character varying(80),
   CONSTRAINT pk_role PRIMARY KEY (idrole)
-)
+);
