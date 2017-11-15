588253baa9e56776549d441c2781644795a61457
Alexandre L'Erario <alerario@gmail.com>
2017-11-15 15:51:37 -0200

    adicionado tabela role em create_tables.sql

--- scripts/database/create_tables.sql
+++ scripts/database/create_tables.sql
@@ -4,3 +4,12 @@
   nome character varying(40),
   CONSTRAINT pk_cid PRIMARY KEY (idcidade)
 )
+
+
+CREATE TABLE public.role
+(
+  idrole integer NOT NULL,
+  nome character varying(30),
+  descricao character varying(80),
+  CONSTRAINT pk_role PRIMARY KEY (idrole)
+)
