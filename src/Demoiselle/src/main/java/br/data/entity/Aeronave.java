69d1fc5bec322e1d38e0e3a9ff5d5ffa8211e0f4
Tamy <tamy.yu@hotmail.com>
2017-12-06 03:54:06 -0200

    criacao das entidades Aeronave e Fabricante

---
+++ src/Demoiselle/src/main/java/br/data/entity/Aeronave.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.data.entity;
+
+/**
+ *
+ * @author Tamy
+ */
+
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "aeronave")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Aeronave.findAll", query = "SELECT a FROM Aeronave a"),
+    @NamedQuery(name = "Aeronave.findById", query = "SELECT a FROM a WHERE a.id = :id"),
+    @NamedQuery(name = "Aeronave.findByModelo", query = "SELECT a FROM Aeronave a WHERE UPPER(FUNCTION('TRANSLATE', a.modelo, "
+                                                    + "'ÁÀÃÂÄáàãâäÍÌÏíìïÚÙÜúùüÉÈÊËéèêëÓÒÕÔÖóòõôöÇç', "
+                                                    + "'AAAAAaaaaaIIIiiiUUUuuuEEEEeeeeOOOOOoooooCc')) LIKE :modelo")})
+public class Aeronave implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name="id")
+    private Integer idaeronave;
+    @Size(max = 50)
+    @Column(name="modelo")
+    private String modelo;
+    @Column (name = "tempo_de_voo")
+    private String tempoVoo;
+    @Column(name="idfabricante")
+    private Integer idfabricante;
+    @Column(name="qtdeassentos")
+    private Integer assentos;
+    @Column(name="ano")
+    private Integer ano;
+    
+    public Aeronave(){}
+    
+    public Aeronave(Integer idaeronave){
+        this.idaeronave = idaeronave;
+    }
+
+    public Integer getIdaeronave() {
+        return idaeronave;
+    }
+
+    public void setIdaeronave(Integer idaeronave) {
+        this.idaeronave = idaeronave;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTempoVoo() {
+        return tempoVoo;
+    }
+
+    public void setTempoVoo(String tempoVoo) {
+        this.tempoVoo = tempoVoo;
+    }
+
+    public Integer getIdfabricante() {
+        return idfabricante;
+    }
+
+    public void setIdfabricante(Integer idfabricante) {
+        this.idfabricante = idfabricante;
+    }
+
+    public Integer getAssentos() {
+        return assentos;
+    }
+
+    public void setAssentos(Integer assentos) {
+        this.assentos = assentos;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+    
+    
+}
---
+++ src/Demoiselle/src/main/java/br/data/entity/Fabricante.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.data.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Tamy
+ */
+
+@Entity
+@Table(name = "aeronave")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name="Fabricante.findAll", query="SELECT f FROM f"),
+    @NamedQuery(name="Fabricante.findById", query="SELECT f FROM f WHERE f.id = :id"),
+    @NamedQuery(name="Fabricante.findByNome", query="SELECT f FROM Fabricante a WHERE UPPER(FUNCTION('TRANSLATE', c.modelo, "
+                                                    + "'ÁÀÃÂÄáàãâäÍÌÏíìïÚÙÜúùüÉÈÊËéèêëÓÒÕÔÖóòõôöÇç', "
+                                                    + "'AAAAAaaaaaIIIiiiUUUuuuEEEEeeeeOOOOOoooooCc')) LIKE :modelo")})
+public class Fabricante implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name="idabricante")
+    private Integer idfabricante;
+    @NotNull
+    @Column(name="nome")
+    private String nome;
+
+    public Integer getIdfabricante() {
+        return idfabricante;
+    }
+
+    public void setIdfabricante(Integer idfabricante) {
+        this.idfabricante = idfabricante;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
