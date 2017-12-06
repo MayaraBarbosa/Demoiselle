6eee08fce22dd7dc45f9b6a99a8321840c852e5c
Lucas Agustini <lucasagustini41@gmail.com>
2017-12-06 00:39:27 -0200

    passagem int

--- src/Demoiselle/src/main/java/br/data/entity/Voo.java
+++ src/Demoiselle/src/main/java/br/data/entity/Voo.java
@@ -6,7 +6,6 @@
 package br.data.entity;
 
 import java.io.Serializable;
-<<<<<<< HEAD
 import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
@@ -19,48 +18,21 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-=======
-import java.util.Objects;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
->>>>>>> 917dcfe04615647f2b26d2284ed6e41f280e10e7
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
-<<<<<<< HEAD
  * @author Lucas Agustini
-=======
- * @author bruno
->>>>>>> 917dcfe04615647f2b26d2284ed6e41f280e10e7
  */
 @Entity
 @Table(name = "voo")
 @XmlRootElement
-<<<<<<< HEAD
 @NamedQueries({
     @NamedQuery(name = "Voo.findAll", query = "SELECT v FROM Voo v")
     , @NamedQuery(name = "Voo.findByIdvoo", query = "SELECT v FROM Voo v WHERE v.idvoo = :idvoo")
     , @NamedQuery(name = "Voo.findByObservacao", query = "SELECT v FROM Voo v WHERE v.observacao = :observacao")
     , @NamedQuery(name = "Voo.findByDatahoraembarque", query = "SELECT v FROM Voo v WHERE v.datahoraembarque = :datahoraembarque")})
 public class Voo implements Serializable {
-
-    @Basic(optional = false)
-    @NotNull
-    //@JoinColumn( name="idaeronave", foreignKey = @ForeignKey(name = "ida")
-    @Column(name = "idaeronave")
-    private int idaeronave;
-    @Column(name = "idpassagem")
-    private Integer idpassagem;
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "idrota")
-    private int idrota;
-    
-    
 
     private static final long serialVersionUID = 1L;
     @Id
@@ -74,27 +46,24 @@ public class Voo implements Serializable {
     @Column(name = "datahoraembarque")
     @Temporal(TemporalType.DATE)
     private Date datahoraembarque;
-=======
-public class Voo implements Serializable {
-    
-    private static final long serialVersionUID = 1L;
-    
-    @Id
-    @NotNull
-    @Column(name = "id")
-    public Integer idvoo;
->>>>>>> 917dcfe04615647f2b26d2284ed6e41f280e10e7
 
+    //@JoinColumn( name="idaeronave", foreignKey = @ForeignKey(name = "ida")
+    @Column(name = "idaeronave")
+    private int idaeronave;
+    @Column(name = "idpassagem")
+    private Integer idpassagem;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idrota")
+    private int idrota;
+    
     public Voo() {
     }
 
-<<<<<<< HEAD
     public Voo(Integer idvoo) {
         this.idvoo = idvoo;
     }
 
-=======
->>>>>>> 917dcfe04615647f2b26d2284ed6e41f280e10e7
     public Integer getIdvoo() {
         return idvoo;
     }
@@ -103,7 +72,6 @@ public class Voo implements Serializable {
         this.idvoo = idvoo;
     }
 
-<<<<<<< HEAD
     public String getObservacao() {
         return observacao;
     }
@@ -124,16 +92,10 @@ public class Voo implements Serializable {
     public int hashCode() {
         int hash = 0;
         hash += (idvoo != null ? idvoo.hashCode() : 0);
-=======
-    @Override
-    public int hashCode() {
-        int hash = 7;
->>>>>>> 917dcfe04615647f2b26d2284ed6e41f280e10e7
         return hash;
     }
 
     @Override
-<<<<<<< HEAD
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Voo)) {
@@ -141,57 +103,56 @@ public class Voo implements Serializable {
         }
         Voo other = (Voo) object;
         if ((this.idvoo == null && other.idvoo != null) || (this.idvoo != null && !this.idvoo.equals(other.idvoo))) {
-=======
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Voo other = (Voo) obj;
-        if (!Objects.equals(this.idvoo, other.idvoo)) {
->>>>>>> 917dcfe04615647f2b26d2284ed6e41f280e10e7
             return false;
         }
         return true;
     }
-<<<<<<< HEAD
 
     @Override
     public String toString() {
         return "br.data.entity.Voo[ idvoo=" + idvoo + " ]";
     }
 
+    /**
+     * @return the idaeronave
+     */
     public int getIdaeronave() {
         return idaeronave;
     }
 
+    /**
+     * @param idaeronave the idaeronave to set
+     */
     public void setIdaeronave(int idaeronave) {
         this.idaeronave = idaeronave;
     }
 
+    /**
+     * @return the idpassagem
+     */
     public Integer getIdpassagem() {
         return idpassagem;
     }
 
+    /**
+     * @param idpassagem the idpassagem to set
+     */
     public void setIdpassagem(Integer idpassagem) {
         this.idpassagem = idpassagem;
     }
 
+    /**
+     * @return the idrota
+     */
     public int getIdrota() {
         return idrota;
     }
 
+    /**
+     * @param idrota the idrota to set
+     */
     public void setIdrota(int idrota) {
         this.idrota = idrota;
     }
-=======
-    
-    
->>>>>>> 917dcfe04615647f2b26d2284ed6e41f280e10e7
     
 }
